eaf0a965cc12ee7cc7799fad7c286849b2705b2d
nicoclos <n.bia@outlook.fr>
2021-10-29 04:27:53 +0200

    initial v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.sentence(word_count: 50 + rand(10)), email: Faker::Internet.email, age: Faker::Number.between(from: 15, to: 99))
+end
+
+10.times do
+    City.create(name: Faker::Address.city, zip_code: Faker::Address.building_number)
+
+end
+
+20.times do
+    Gossip.create(
+        title: Faker::Lorem.sentence(word_count: 10), content: Faker::Lorem.sentence(word_count: 50 + rand(10))
+    )
+end
